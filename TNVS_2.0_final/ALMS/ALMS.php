e58def782cd150fcb8c2be9020ecf4ed710a698b
M0nsk1e <andreimacanas5@example.com>
2025-08-14 23:08:49 +0800

    afsf

--- TNVS_2.0_final/ALMS/ALMS.php
+++ TNVS_2.0_final/ALMS/ALMS.php
@@ -38,7 +38,15 @@
     <div class="p-3 border-top mb-2">
       <a class="nav-link text-danger" href="/login.php">
         <ion-icon name="log-out-outline"></ion-icon> Logout
+
+
+
+
+
+        
+      </a>
       </a>
     </div>
   </div>
 </div>
+a
