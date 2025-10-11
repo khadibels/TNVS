4943afbfcc5eb4f1c39f8e4830ec389d8e9021de
khadibels <nicolemalitao08@gmail.com>
2025-10-11 12:29:14 +0800

    update sidebar

--- procurement/inventoryView.php
+++ procurement/inventoryView.php
@@ -10,6 +10,9 @@ $catNames = $pdo->query("SELECT name FROM inventory_categories WHERE active=1 OR
                ->fetchAll(PDO::FETCH_COLUMN);
 
 
+$section = 'procurement';
+$active = 'po_inventory';
+
 $userName = "Procurement User";
 $userRole = "Procurement";
 if (function_exists('current_user')) {
@@ -36,30 +39,7 @@ if (function_exists('current_user')) {
 <div class="container-fluid p-0">
   <div class="row g-0">
 
-    <!-- Procurement sidebar -->
-    <div class="sidebar d-flex flex-column">
-      <div class="d-flex justify-content-center align-items-center mb-4 mt-3">
-        <img src="../img/logo.png" id="logo" class="img-fluid me-2" style="height:55px" alt="Logo">
-      </div>
-
-      <h6 class="text-uppercase mb-2">Procurement</h6>
-      <nav class="nav flex-column px-2 mb-4">
-        <a class="nav-link" href="./procurementDashboard.php"><ion-icon name="home-outline"></ion-icon><span>Dashboard</span></a>
-        <a class="nav-link" href="./supplierManagement.php"><ion-icon name="person-outline"></ion-icon><span>Supplier Management</span></a>
-        <a class="nav-link" href="./rfqManagement.php"><ion-icon name="mail-open-outline"></ion-icon><span>RFQs & Sourcing</span></a>
-        <a class="nav-link" href="./purchaseOrders.php"><ion-icon name="document-text-outline"></ion-icon><span>Purchase Orders</span></a>
-        <a class="nav-link" href="./procurementRequests.php"><ion-icon name="clipboard-outline"></ion-icon><span>Procurement Requests</span></a>
-        <a class="nav-link active" href="./inventoryView.php"><ion-icon name="archive-outline"></ion-icon><span>Inventory Management</span></a>
-        <a class="nav-link" href="./budgetReports.php"><ion-icon name="analytics-outline"></ion-icon><span>Budget & Reports</span></a>
-        <a class="nav-link" href="./settings.php"><ion-icon name="settings-outline"></ion-icon><span>Settings</span></a>
-      </nav>
-
-      <div class="logout-section">
-        <a class="nav-link text-danger" href="<?= defined('BASE_URL') ? BASE_URL : '#' ?>/auth/logout.php">
-          <ion-icon name="log-out-outline"></ion-icon> Logout
-        </a>
-      </div>
-    </div>
+    <?php include __DIR__ . '/../includes/sidebar.php' ?>
 
     <!-- Main -->
     <div class="col main-content p-3 p-lg-4">
